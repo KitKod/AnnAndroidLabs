2f386cc4758ed86c9ce9b0620461280ec3a3d270
Mykyta Kamak <mykytakamak@gmail.com>
2021-09-26 15:10:08 +0300

    Add button

--- lab1/app/src/main/java/com/example/lab1/MainActivity.kt
+++ lab1/app/src/main/java/com/example/lab1/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.lab1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun clickOnShowMsg(view: View) {
+        var helloTextView = findViewById<TextView>(R.id.Msg_textView)
+        helloTextView.visibility = if (helloTextView.visibility == View.VISIBLE){
+            View.INVISIBLE
+        } else{
+            View.VISIBLE
+        }
+
     }
 }
