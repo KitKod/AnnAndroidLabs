f1a48cbf8fb07c46371e455275c7ae6891107d81
Mykyta Kamak <mykytakamak@gmail.com>
2021-11-13 15:52:15 +0200

    Add image

--- Lab_1/app/src/main/java/com/example/lab_1/MainActivity.java
+++ Lab_1/app/src/main/java/com/example/lab_1/MainActivity.java
@@ -3,12 +3,38 @@ package com.example.lab_1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView helloTextView;
+    private ImageButton changeTextBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        changeTextBtn = findViewById(R.id.imageButton);
+        changeTextBtn.setOnClickListener(getCliclListener());
+
+        helloTextView = findViewById(R.id.textView2);
     }
+
+    private View.OnClickListener getCliclListener() {
+        return new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (helloTextView.getText().toString().equals("")) {
+                    helloTextView.setText("Hello Friend!!!");
+                } else {
+                    helloTextView.setText("");
+                }
+
+            }
+        };
+    }
+
 }
